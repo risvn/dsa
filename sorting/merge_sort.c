85267d24dd84536635d58597ba664fecc1b57fd9
rsvn <rizwanshaz2004@gmail.com>
2025-11-25 12:40:10 +0530

    fix:merge_sort

--- sorting/merge_sort.c
+++ sorting/merge_sort.c
@@ -7,68 +7,74 @@ int len;
 } list;
 
 
+list* split(list* elements,int start_index,int end_index){
+
+
+    if (start_index < 0 || end_index >= elements->len || start_index > end_index)
+    return NULL;
+
+   int count = end_index - start_index + 1;
+   int* new_arr = malloc(count * sizeof(int));
+    if (!new_arr) return NULL;
+
+  for(int i=0;i<count;i++){
+      new_arr[i]=elements->arr[start_index+i];
+  }
+  list* tmp=malloc(sizeof(list));
+  tmp->arr=new_arr;
+  tmp->len = count;
+  return tmp;
+}
 
 
 
-int merge(list left,list right,list result){
+int merge(list* left,list* right,list* result){
   int i=0,j=0,k=0;
-  while(i<left.len && j<right.len){
-    if(left.arr[i]<right.arr[j]){
-      result.arr[k++]=left.arr[i++];
+  while(i<left->len && j<right->len){
+    if(left->arr[i]<right->arr[j]){
+      result->arr[k++]=left->arr[i++];
     }
 
     else{
-      result.arr[k++]=right.arr[j++];
+      result->arr[k++]=right->arr[j++];
     }
 
   }
 
-    while(i<left.len){
-    result.arr[k++]=left.arr[i++];
+    while(i<left->len){
+    result->arr[k++]=left->arr[i++];
     }
   
-    while(j<right.len){
-    result.arr[k++]=left.arr[j++];
+    while(j<right->len){
+    result->arr[k++]=right->arr[j++];
     }
   
-  result.len=k;
+  result->len=k;
   return 0;
 }
 
 
-void merge_sort(list elements){
+void merge_sort(list* elements){
   
   
-    if (elements.len < 2)
+    if (elements->len < 2)
         return;
 
-    int mid = elements.len / 2;
- // allocate left and right subarrays
-    list left, right;
-    left.len = mid;
-    right.len = elements.len - mid;
-
-    left.arr = malloc(left.len * sizeof(int));
-    right.arr = malloc(right.len * sizeof(int));
-
-    // fill left
-    for (int i = 0; i < mid; i++)
-        left.arr[i] = elements.arr[i];
-
-    // fill right
-    for (int i = mid; i < elements.len; i++)
-        right.arr[i - mid] = elements.arr[i];
-
-
+  int mid = elements->len/2;
+   list* left=split(elements,0,mid-1); 
+   list* right=split(elements,mid,elements->len-1); 
+  
   merge_sort(left);
   merge_sort(right);
   merge(left,right,elements);
 
-  // free temp arrays
-    free(left.arr);
-    free(right.arr);
+   // free allocated memory
+    free(left->arr);
+    free(left);
+    free(right->arr);
+    free(right);
 
-}
+  }
 
 int main() {
     int raw[] = {8, 3, 1, 7, 4, 9, 2, 6};
@@ -83,7 +89,7 @@ int main() {
         printf("%d ", elements.arr[i]);
     printf("\n");
 
-    merge_sort(elements);
+    merge_sort(&elements);
 
     printf("After sort:  ");
     for (int i = 0; i < n; i++)
