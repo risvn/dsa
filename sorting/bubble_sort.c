ac0a882f9df06a2e951de58bedbad84965304b57
rsvn <rizwanshaz2004@gmail.com>
2025-12-23 15:51:00 +0530

    sorting algos

--- sorting/bubble_sort.c
+++ sorting/bubble_sort.c
@@ -1,25 +1,27 @@
 //====================Bubble Sort====================//
 #include<stdio.h>
 
-int bubble_sort(int* arr,int len)
-{
-  for(int j=0;j<=len;j++){
-  for(int i=0;i<=len-j;i++){
-      if(arr[i]>arr[i+1]){
-     int temp=arr[i+1];
-      arr[i+1]=arr[i];
-      arr[i]=temp;
+int* bubble(int* arr,int len){
+  for(int i=0;i<len;i++){
+  for(int j=0;j<len-i-1;j++){
+  if(arr[j]>arr[j+1]){
+  int tmp=arr[j];
+  arr[j]=arr[j+1];
+  arr[j+1]=tmp;
+
+       }
       }
   }
-}
-return 0;
-}
-int main(){
-  int arr[5]={1,5,4,2,6};
-  int len=((sizeof(arr)/sizeof(int))-1);
-  bubble_sort(arr,len);
-  for(int i=0;i<=len;i++){
-      printf("sorted:%d\n",arr[i]);
-      }
-}
+return arr;
+  }
 
+int main(){
+  int arr[5]={2,5,3,8,1};
+  int len=sizeof(arr)/sizeof(arr[0]);
+  int* sorted=bubble(arr,len);
+  for(int i=0;i<len;i++){
+    printf("%d\n",sorted[i]);
+
+
+  }
+}
